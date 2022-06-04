814361098295a41b519bc4e47a4396b89e417b73
Estefano Capacyachi Gonzales <estefano.gonzales1@gmail.com>
2022-06-03 22:28:03 -0500

    ApiGateway config

---
+++ src/main/java/com/idat/pe/ApgwServerFinalApplication.java
@@ -0,0 +1,13 @@
+package com.idat.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApgwServerFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgwServerFinalApplication.class, args);
+	}
+
+}
